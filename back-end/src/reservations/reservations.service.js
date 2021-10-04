6231189766472e3554eb7dfe3d23640d1ffcaebe
Leana Lopez <chileana_go@yahoo.com>
2021-10-03 22:38:03 -0700

    added reservations.service.js

---
+++ back-end/src/reservations/reservations.service.js
@@ -0,0 +1,46 @@
+const knex = require("../db/connection");
+
+function list() {
+  return knex("reservations")
+    .select("*")
+    .orderBy("reservation_date")//, "reservation_time");
+}
+
+function listByDate(date) {
+    return knex("reservations")
+      .select("*")
+      .where({ reservation_date: date })
+      .orderBy("reservation_time");
+  }
+
+function create(reservation) {
+  return knex("reservations")
+    .insert(reservation)
+    .returning("*")
+    .then((createdRecords) => createdRecords[0]);
+}
+
+function read(reservation_id) {
+  return knex("reservations").select("*").where({ reservation_id }).first();
+}
+
+function update(updatedreservation) {
+  return knex("reservations")
+    .select("*")
+    .where({ reservation_id: updatedreservation.reservation_id })
+    .update(updatedreservation, "*")
+    .then((updatedRecords) => updatedRecords[0]);
+}
+
+function destroy(reservation_id) {
+  return knex("reservations").where({ reservation_id }).del();
+}
+
+module.exports = {
+  list,
+  listByDate,
+  create,
+  read,
+  update,
+  delete: destroy,
+};
