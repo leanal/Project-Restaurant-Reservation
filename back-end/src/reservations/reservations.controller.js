a1873ac2874a4a020c54437c81737f1479678947
Leana Lopez <chileana_go@yahoo.com>
2021-10-04 22:23:15 -0700

    added input validations

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,6 +1,7 @@
 const reservationsService = require("./reservations.service.js");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const hasProperties = require("../errors/hasProperties");
+const { NotificationCenter } = require("node-notifier");
 const hasRequiredProperties = hasProperties(
   "first_name",
   "last_name",
@@ -10,7 +11,6 @@ const hasRequiredProperties = hasProperties(
   "people"
 );
 
-// 400! The reservation date is a Tuesday. The restaurant is closed on Tuesdays.
 // 400! The reservation date is in the past. Only future reservations are allowed. time & date
 // Hint Parsing a Date that includes the time in JavaScript can be tricky. Again, keep an eye out for which time zone is being used for your Dates.
 // { status: 404, message: `The reservation time cannot be before 10:30 AM.` }
@@ -43,6 +43,75 @@ function hasOnlyValidProperties(req, res, next) {
   next();
 }
 
+/**
+ * Validates input for people,
+ * @param { body.data } req
+ * @param {*} next
+ * @returns property names of the invalid inputs
+ */
+function hasValidInputs(req, res, next) {
+  const { people, reservation_date, reservation_time } = req.body.data;
+  let invalidInputs = "Invalid input(s):";
+
+  if (typeof people !== "number") {
+    invalidInputs = invalidInputs.concat(" people");
+  }
+  if (!reservationDateIsValid(reservation_date)) {
+    invalidInputs = invalidInputs.concat(" reservation_date");
+  }
+
+  if (!reservationTimeIsValid(reservation_time)) {
+    invalidInputs = invalidInputs.concat(" reservation_time");
+  }
+
+  if (invalidInputs !== "Invalid input(s):") {
+    return next({
+      status: 400,
+      message: invalidInputs,
+    });
+  }
+
+  if (reservationDateIsTuesday(reservation_date)) {
+    return next({
+      status: 400,
+      message: `The restaurant is closed on Tuesdays.`,
+    });
+  }
+
+  if (!reservationDateIsInTheFuture(reservation_date)) {
+    return next({
+      status: 400,
+      message: `Please enter a future reservation date.`,
+    });
+  }
+  return next();
+}
+
+// checks if the reservation_date is a valid Date
+function reservationDateIsValid(reservation_date) {
+  const timestamp = Date.parse(reservation_date);
+  if (isNaN(timestamp) == false) {
+    const date = new Date(timestamp);
+    return date instanceof Date ? true : false;
+  }
+}
+
+function reservationTimeIsValid(reservation_time) {
+  const isTime = reservation_time.match(/^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/);
+  return isTime ? true : false;
+}
+
+// checks if the reservation_date is a Tuesday
+function reservationDateIsTuesday(reservation_date) {
+  const timestamp = Date.parse(`${reservation_date} PST`);
+  const date = new Date(timestamp);
+  return date.getDay() == 2 ? true : false;
+}
+
+function reservationDateIsInTheFuture(reservation_date) {
+  const reservationDateTimestamp = Date.parse(`${reservation_date} PST`);
+  return reservationDateTimestamp > Date.now()
+}
 async function create(req, res) {
   const data = await reservationsService.create(req.body.data);
   res.status(201).json({ data });
@@ -78,12 +147,10 @@ async function destroy(req, res) {
 async function list(req, res) {
   const { date } = req.query;
   let data = [];
-  
+
   if (date) {
-    console.log("listByDate");
     data = await reservationsService.listByDate(date);
   } else {
-    console.log("list");
     data = await reservationsService.list();
   }
 
@@ -97,6 +164,7 @@ module.exports = {
   create: [
     hasOnlyValidProperties,
     hasRequiredProperties,
+    hasValidInputs,
     asyncErrorBoundary(create),
   ],
   update: [
--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -8,7 +8,12 @@ const hasRequiredProperties = hasProperties("table_name", "capacity");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 const VALID_PROPERTIES = ["table_name", "capacity"];
-
+/**
+ * Validates the request properties
+ * @param req
+ * @returns {<Error>}
+ * If the request body has an invalid property, returns 400 and the invalid field(s)
+ */
 function hasOnlyValidProperties(req, res, next) {
   const { data = {} } = req.body;
 
@@ -22,10 +27,41 @@ function hasOnlyValidProperties(req, res, next) {
       message: `Invalid field(s): ${invalidFields.join(", ")}`,
     });
   }
+
   next();
 }
 
+function hasValidInputs(req, res, next) {
+  const { table_name, capacity } = req.body.data
+  let invalidInputs = "Invalid input(s):";
+
+  if (table_name.length < 2) {
+    invalidInputs = invalidInputs.concat(" table_name");
+    // return next({
+    //   status: 400,
+    //   message: `"table_name" must have more than two characters.`,
+    // });
+  }
+  
+  if (typeof capacity !== "number"){
+    invalidInputs = invalidInputs.concat(" capacity");
+  }
+  
+  if (invalidInputs !== "Invalid input(s):") {
+    return next({
+      status: 400,
+      message: invalidInputs,
+    });
+  }
+
+  next()
+}
+
+// creates a new 'table' with a reservation_id property
 async function create(req, res) {
+  // const { table_name, capacity } = req.body.data
+  // console.log(req.body.data);
+  // const newTable = {...req.body.data, reservation_id: null }
   const data = await tablesService.create(req.body.data);
   res.status(201).json({ data });
 }
@@ -35,25 +71,36 @@ async function list(req, res, next) {
   res.json({ data: data });
 }
 
-// async function tableExists(req, res, next) {
-//   const table = await tablesService.read(req.params.tableId);
-//   if (table) {
-//     res.locals.table = table;
-//     next();
-//   }
-// }
+async function tableExists(req, res, next) {
+  const table = await tablesService.read(req.params.tableId);
+  if (table) {
+    res.locals.table = table;
+    next();
+  }
+}
 
-// async function read(req, res) {
-//   const data = res.locals.table;
-//   res.json({ data: data });
-// }
+
+async function update(req, res) {
+    const updatedTable = {
+      ...req.body.data,
+      table_id: res.locals.table.table_id,
+    };
+    const data = await tablesService.update(updatedTable);
+    res.json({ data });
+  }
+
+async function read(req, res) {
+  const data = res.locals.table;
+  res.json({ data: data });
+}
 
 module.exports = {
   create: [
     hasOnlyValidProperties,
     hasRequiredProperties,
+    hasValidInputs,
     asyncErrorBoundary(create),
   ],
   list: [asyncErrorBoundary(list)],
-//   read: [tableExists, asyncErrorBoundary(read)],
+  read: [asyncErrorBoundary(tableExists), asyncErrorBoundary(read)],
 };
