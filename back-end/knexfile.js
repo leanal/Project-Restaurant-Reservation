48b8a57dba9799f7368cf834512cd3dc47a7da0f
Leana Lopez <chileana_go@yahoo.com>
2021-10-23 15:02:16 -0700

    added url to knex

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -10,6 +10,9 @@ const path = require("path");
 const {
   DATABASE_URL = "postgres://juunlpgj:cT2DoDfJknYYcKQVHdi-_IGF6umskeij@kashin.db.elephantsql.com/juunlpgj",
   DATABASE_URL_DEVELOPMENT = "postgres://emgawehe:3w9O43g-GsfbdK7g4kMxM_9Z21TMowDA@kashin.db.elephantsql.com/emgawehe",
+  DATABASE_URL_TEST = "postgres://xvoxbddq:u792bEhbP2SGyAXR6iggRoLlAJQAR6cj@kashin.db.elephantsql.com/xvoxbddq",
+  DATABASE_URL_PREVIEW = "postgres://ocjqglkm:3jsKfcdXiROB3DOcyzxBd1-q2u2GsIDI@kashin.db.elephantsql.com/ocjqglkm",
+  DEBUG,
   // DATABASE_URL = "postgresql://postgres@localhost/postgres",
   // DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
   // DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
